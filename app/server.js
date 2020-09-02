641077b84a96252d55ad012ce3c1d1e4e7a52251
Xtrendence <xtrendence@yahoo.com>
2020-09-02 14:35:46 +0100

    Implemented Electron.

--- app/server.js
+++ app/server.js
@@ -1,21 +1,58 @@
-const port = 1999;
+const localPort = 1999;
+const appPort = 1998;
 const scriptPath = process.cwd() + "\\server.js";
 
+const electron = require("electron");
+
 const express = require("express");
-const app = express();
-const server = app.listen(port);
+const localExpress = express();
+const appExpress = express();
+const localServer = localExpress.listen(localPort, "localhost");
+const appServer = appExpress.listen(appPort);
 
 const fs = require("fs");
 const path = require("path");
 const body_parser = require("body-parser");
 
 const dataDirectory = path.join(__dirname, "./data/");
+const settingsFile = dataDirectory + "settings.json";
+const playlistsFile = dataDirectory + "playlists.json";
 
-app.set("view engine", "ejs");
-app.use("/assets", express.static("assets"));
-app.use(body_parser.urlencoded({ extended:true }));
-app.use(body_parser.json({ limit:"512mb" }));
+const { app, BrowserWindow, screen, ipcMain } = electron;
 
-app.get("/", (req, res) => {
-	res.render("index");
+app.on("ready", function() {
+	const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+	
+	let windowWidth = 1280;
+	let windowHeight = 720;
+
+	if(width <= 1080 || height <= 620) {
+		windowWidth = width - 100;
+		windowHeight = height - 100;
+	}
+
+	const localWindow = new BrowserWindow({
+		width:windowWidth,
+		height:windowHeight,
+		resizable:true,
+		frame:false,
+		webPreferences: {
+			nodeIntegration:true
+		}
+	});
+
+	localWindow.loadURL("http://127.0.0.1:" + localPort);
+
+	localExpress.set("view engine", "ejs");
+	localExpress.use("/assets", express.static("assets"));
+	localExpress.use(body_parser.urlencoded({ extended:true }));
+	localExpress.use(body_parser.json({ limit:"512mb" }));
+
+	localExpress.get("/", (req, res) => {
+		res.render("index");
+	});
+
+	ipcMain.on("getInfo", function(error, req) {
+
+	});
 });

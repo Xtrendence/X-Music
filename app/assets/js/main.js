dab7262cb97c16cb265c7f579158caa4969dcf57
Xtrendence <xtrendence@yahoo.com>
2020-09-10 17:35:10 +0100

    Audio player will now hide if the library directory is changed.

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -203,6 +203,7 @@ document.addEventListener("DOMContentLoaded", () => {
 			let settings = JSON.parse(res.settings);
 			inputLibraryDirectory.value = settings.libraryDirectory;
 			if(libraryDirectory !== settings.libraryDirectory) {
+				hideAudioPlayer();
 				libraryDirectory = settings.libraryDirectory;
 				getSongs();
 			}
@@ -367,6 +368,16 @@ document.addEventListener("DOMContentLoaded", () => {
 			element.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M504 256c0 136.997-111.043 248-248 248S8 392.997 8 256C8 119.083 119.043 8 256 8s248 111.083 248 248zm-248 50c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z"/></svg><span class="title">No Playlists Found...</span>';
 			divListview.appendChild(element);
 		}
+	}
+
+	function hideAudioPlayer() {
+		currentSong = "";
+		audioFile.pause();
+		audioFile.src = "";
+		divListview.classList.remove("active");
+		spanAudioBanner.classList.add("hidden");
+		divAudioPlayer.classList.add("hidden");
+		showPlay();
 	}
 
 	function playSong(file, song) {
